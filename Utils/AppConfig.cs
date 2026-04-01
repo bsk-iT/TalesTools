032af3572659084d09c8405c8d6c449c270f9201
bsk-iT <bruno.cubero10@gmail.com>
2026-04-01 13:16:20 -0300

    chore: update gitignore and fix AppConfig links to own fork

--- Utils/AppConfig.cs
+++ Utils/AppConfig.cs
@@ -5,13 +5,15 @@ namespace _4RTools.Utils
 {
     internal class AppConfig
     {
-        public static string Name = "TalesTools";
+        public static string Name = "BSKTools";
         public static string ProfileFolder = "Profile\\";
+        public static string LastProfileFile = "lastprofile.txt";
         public static string Website = "https://ragnatales.com.br/";
-        public static string GithubLink = "https://github.com/biancaazuma/TalesTools/tree/main";
+        public static string GithubLink = "https://github.com/bsk-iT/TalesTools/tree/main";
         public static string DiscordLink = "https://discord.com/invite/bkG2RPGjEu";
-        public static string _4RLatestVersionURL = "https://api.github.com/repos/biancaazuma/TalesTools/releases/latest";
-        public static string Livepix = "https://livepix.gg/hannamori";
-        public static string Version = "v3.1.3";
+        public static string _4RLatestVersionURL = "https://api.github.com/repos/bsk-iT/TalesTools/releases/latest";
+        public static string LivepixHannamori = "https://livepix.gg/hannamori";
+        public static string LivepixBerserk = "https://livepix.gg/theberserk";
+        public static string Version = "v5.2.1";
     }
 }
